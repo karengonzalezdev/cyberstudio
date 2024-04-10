1f4a71f3c5427f492300f354fe22c5caa5d0cd05
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-04-10 12:07:37 -0600

    Updated contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,10 @@
 "use client"
 import { motion } from "framer-motion"
 import { useRef, useState } from "react";
-{/*
 import emailjs from '@emailjs/browser';
-*/}
 
 const ContactPage = () => {
 
-{/*
 const [success,setSuccess] = useState(false);
 const [error,setError] = useState(false);
 const text = "Hello there! ";
@@ -33,34 +30,33 @@ const sendEmail = (e) => {
         }
       );
   };
-*/}
 
  return (
-    <motion.div className="h-full overflow-scroll lg:overflow-hidden xl:overflow-hidden" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
-    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-3xl lg:text-6xl xl:text-6xl">
-            <div className="">hola {/* {text.split("").map((letter, index)=>(
-                <motion.span key={index} initial={{opacity:1}} animate={{opacity:0}} transition={{duration:3, repeat:Infinity, delay:index*0.1}}>{letter}</motion.span>
-            ))} */} 😊</div>
-        </div>
-        {/* FORM CONTAINER */}
-        <form
-           /* onSubmit={sendEmail} */
-            /*ref={form}*/ className="h-[80%] lg:h-full lg:w-1/2 bg-purple-50 rounded-xl text-lg lg:text-xl xl:text-xl flex flex-col gap-8 justify-center p-10 sm:p-18 md:p-24 lg:p-24 xl:p-24">
-            <span className="pt-5">Equipo de Cyberstudio,</span>
-            <textarea rows={10} className="bg-transparent border-b-2 border-b-black outline-none resize-none min-h-[2rem]"
-            name="user_message"
-            />
-            <span>Mi correo electrónico es:</span>
-            <input name="user_email" type="text" className="bg-transparent border-b-2 border-b-black outline-none" />
-            <span>Saludos!</span>
-            <button className="bg-purple-200 rounded font-semibold text-gray-600 lg:p-4 xl:p-4">Enviar</button>
-             {/*{success && */ <span className="text-green-600 font-semibold">Tu mensaje se envió correctamente!</span>}
-            {/* {error && */ <span className="text-red-600 font-semibold">Hubo un problema al enviar tu mensaje, por favor reinténtalo.</span>}
-        </form>
-    </div>
-    </motion.div>
+  <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 pb-5 pt-5 items-center justify-center">
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-3xl lg:text-6xl xl:text-6xl pt-3 lg:pt-0 pb-3 lg:pb-0 text-glitch text-glitch-duration-slow">
+          <div className="">{text.split("").map((letter, index)=>(
+              <motion.span key={index} initial={{opacity:1}} animate={{opacity:0}} transition={{duration:3, repeat:Infinity, delay:index*0.1}}>{letter}</motion.span>
+          ))} :) </div>
+      </div>
+      {/* FORM CONTAINER */}
+      <form
+          onSubmit={sendEmail} 
+          ref={form} className="h-[80%] lg:h-full md:w-1/2 lg:w-1/2 bg-purple-50 rounded-xl text-lg lg:text-xl xl:text-xl flex flex-col gap-8 justify-center px-10 sm:px-12 md:px-15 pt-10 md:pt-15 lg:px-20 lg:pt-10 pb-20 md:pb-34">
+          <span className="pt-5 lg:pt-2">Equipo de Cyberstudio,</span>
+          <textarea rows={4} className="bg-transparent border-b-2 border-b-black outline-none resize-none min-h-[2rem]"
+          name="user_message"
+          />
+          <span>Mi correo electrónico es:</span>
+          <input name="user_email" type="text" className="bg-transparent border-b-2 border-b-black outline-none" />
+          <span>Saludos!</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 lg:p-4 xl:p-4">Enviar</button>
+          {success && <span className="text-green-600 font-semibold">Tu mensaje fue enviado correctamente!</span>}
+          {error && <span className="text-red-600 font-semibold">Hubo un error al enviar tu mensaje, porfavor intenta nuevamente</span>}
+      </form>
+  </div>
+  </motion.div>
 )
 }
 
