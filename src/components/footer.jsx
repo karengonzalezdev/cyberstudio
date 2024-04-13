77daa91387581ff6ee270584c64436248ab14aa2
Karen Gonzalez <karenfggutierrez@gmail.com>
2024-04-12 20:01:44 -0600

    Linked social media for footer page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => {
  <div className="h-[13rem] bg-violet-950 relative">
 {/* SOCIAL */}
 <div className="flex w-full gap-5 pt-5 justify-center">
-<Link href="https://github.com/karenfggutierrez" className="pt-1.5 pr-1">
+<Link href="https://www.facebook.com/profile.php?id=100077478523775" target="_blank" className="pt-1.5 pr-1">
 <img src="./facebook.png" alt="Facebook" width={35} height={35}/>
 </Link>
-<Link href="/" className="pt-1.5">
+<Link href="https://www.instagram.com/cyberstudio3?igsh=bGdldWVhNHNhb3d2" target="_blank" className="pt-1.5">
 <img src="./instagram.png" alt="Instagram" width={35} height={35}/>
 </Link>
-<Link href="/">
+<Link href="https://wa.me/523339796806" target="_blank">
 <img src="./whatsapp.png" alt="Whatsapp" width={46} height={46}/>
 </Link>
-<Link href="/" className="pt-1">
+<Link href="mailto:cyberstudio321@gmail.com" target="_blank" className="pt-1">
 <img src="./email.png" alt="Email" width={35} height={55}/>
 </Link>
 </div>
